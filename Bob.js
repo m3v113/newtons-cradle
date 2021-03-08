28d5194f68c1524cfcf5f022e497bf686210a31c
m3v113 <76668066+m3v113@users.noreply.github.com>
2021-03-08 08:28:14 -0800

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,26 @@
+class Bob{
+    constructor(x, y, radius) {
+
+        var options = {
+            isStatic: true,
+            restituition: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+        
+        this.body = Bodies.circle(x, y, radius, options);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+
+    display() {
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        fill("lightblue");
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.radius);
+        pop();
+    }
+}
---
+++ Rope.js
@@ -0,0 +1,34 @@
+class Rope {
+    constructor(bodyA, bodyB, OffsetX, OffsetY)
+    {
+        this.offsetX = OffsetX;
+        this.offsetY = OffsetY;
+
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            pointB:{x:this.offsetX, y:this.offsetY},
+            length: 10,
+            stiffness: 0.04
+     }
+
+     this.rope = Constraint.create(options);
+     World.add(world, this.rope);
+              
+ }   
+    display() {
+  
+        //this point is referring to bodyA
+        var pointA = this.rope.bodyA.position;
+        //this point is referring to bodyB
+        var pointB = this.rope.bodyB.position;
+        push();
+        strokeWeight(6);
+        line(pointA.x, pointA.y, pointB.x + this.offsetX, pointB.y + this.offsetY);
+        //line(x1,y1,x2,y2);
+        pop();
+        
+        
+    }
+
+}
